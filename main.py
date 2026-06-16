ee2bf40a91c334648a160cafcd032aee071bd1e2
Wimpy57 <venigretdj@gmail.com>
2026-06-17 01:09:01 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+from aiogram.utils import executor
+
+import config
+
+
+APP_TOKEN = config.TOKEN
