5e5f2d661243379afd540f2804250310d204c215
Wimpy57 <venigretdj@gmail.com>
2026-06-17 01:43:17 +0300

    Copied code from tutorial (not working for the newest version)

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
-from aiogram.utils import executor
 
 import config
+import asyncio
 
 
 APP_TOKEN = config.TOKEN
+bot = Bot(token=APP_TOKEN)
+dp = Dispatcher()
+
+@dp.message()
+async def start(message:types.Message):
+    web_app = WebAppInfo(url='asdfasdf')
+    button = KeyboardButton(text='Открыть веб-приложение', web_app=web_app)
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(button)
+    await message.answer("Нажмите кнопку ниже для запуска веб-приложения", reply_markup=keyboard)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
